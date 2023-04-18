01064a801ff9236cc281d2ebecbab4ecff4db985
joshuapent <joshdavidpent@gmail.com>
2023-04-17 22:21:10 -0500

    Added a bunch of changes to the setup

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
