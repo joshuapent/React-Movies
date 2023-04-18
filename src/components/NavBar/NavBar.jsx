de4f045dd64d00f8824852dfb44248dacb81ba03
joshuapent <joshdavidpent@gmail.com>
2023-04-17 23:22:29 -0500

    Finished part 1

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function NavBar() {
+function NavBar(props) {
   return (
-    <div>NavBar</div>
+    <nav>
+        <Link to="/">Movies</Link> &nbsp; | &nbsp; {' '}
+        <Link to="/actors">Actors</Link> &nbsp; | &nbsp; {' '}
+        {props.user}
+    </nav>
   )
 }
 
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar';
 import LoginPage from '../LoginPage/LoginPage';
+import MoviesListPage from '../MoviesListPage/MoviesListPage';
+import MovieDetailPage from '../MovieDetailPage/MovieDetailPage';
+import ActorListPage from '../ActorListPage/ActorListPage';
 
 function App() {
 
@@ -11,15 +14,19 @@ function App() {
 
   return (
     <div className="App">
-      <h1>App</h1>
       { user ? (
         <>
-          < NavBar />
+          < NavBar user={user} />
           <Routes>
+            <Route path="/" element={< MoviesListPage/>} />
+            <Route path="/movies/:movieName" element={< MovieDetailPage/>} />
+            <Route path="/actors" element={< ActorListPage/>} />
           </Routes>
         </>
       ) : (
-        < LoginPage setUser={setUser} />
+        <Routes>
+          < Route path="/" element={<LoginPage setUser={setUser} />} />
+        </Routes>
       )
       }
     </div>
